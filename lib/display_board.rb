00d49ca743d57d06b21d981610135ac57feeed96
Don Ward <10is0420@gmail.com>
2019-02-22 12:59:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board (board)
-  puts "   |   |   "
+  puts " #{board[0]} |   |   "
   puts "-----------"
   puts "   | #{board[4]} |   "
   puts "-----------"
