fa049aa1c4c044f7cf723f556b977b47e21c1c44
Don Ward <10is0420@gmail.com>
2019-02-22 12:58:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 def display_board (board)
   puts "   |   |   "
   puts "-----------"
-  puts "   | #{board[5]} |   "
+  puts "   | #{board[4]} |   "
   puts "-----------"
   puts "   |   |   "
 end
